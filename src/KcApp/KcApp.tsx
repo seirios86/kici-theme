4ff9a5f047cd83b4ee9fc40cf9f5482d595ce458
harvey.c <harvey.c@kakaoenterprise.com>
2022-08-24 21:12:42 +0900

    Complete login page

--- src/KcApp/KcApp.tsx
+++ src/KcApp/KcApp.tsx
@@ -19,24 +19,19 @@ export const KcApp = memo(({ kcContext }: { kcContext: KcContext; }) => {
     const kcProps = {
         ...defaultKcProps,
         "kcHtmlClass": [
-            ...defaultKcProps.kcHtmlClass,
-            classes.kcHtmlClass
+            ...defaultKcProps.kcHtmlClass, classes.kcHtmlClass
         ],
         "kcFormCardClass": [
-            ...defaultKcProps.kcFormCardClass,
-            classes.kcFormCardClass
+            ...defaultKcProps.kcFormCardClass, classes.kcFormCardClass
         ],
         "kcFormHeaderClass": [
-            ...defaultKcProps.kcFormHeaderClass,
-            classes.kcFormHeaderClass
+            ...defaultKcProps.kcFormHeaderClass, classes.kcFormHeaderClass
         ],
         "kcButtonPrimaryClass": [
-            // ...defaultKcProps.kcButtonPrimaryClass,
-            classes.kcButtonPrimaryClass,
+            ...defaultKcProps.kcButtonPrimaryClass, classes.kcButtonPrimaryClass,
         ],
         "kcLoginClass": [
-            ...defaultKcProps.kcLoginClass,
-            classes.kcLoginClass,
+            ...defaultKcProps.kcLoginClass, classes.kcLoginClass,
         ],
     };
 
@@ -54,52 +49,77 @@ export const KcApp = memo(({ kcContext }: { kcContext: KcContext; }) => {
 
 const useStyles = makeStyles({ "name": { KcApp }})(theme => ({
     "kcHtmlClass": {
-        "& body": {
-            "background": `no-repeat center center fixed`,
-        },
         "background": `#fff`,
-        "& #kc-header": {
-            "color": `#222222`,
+        "body": {
+            "background": `no-repeat center center fixed`,
+            "color": `#222`,
         },
-        "& #kc-header-wrapper": {
-            "textTransform": "none",
-        },
+        "#kc-header-wrapper": {
+            "span": {
+                "display": `none`,
+            }
+        }
     },
     "kcFormCardClass": {
         "border": `1px solid rgba(0,0,0,0.1)`,
         "borderRadius": `8px`,
+        ".kc-feedback-text": {
+            "color": `#222`,
+            "fontSize": `14px`,
+        },
+        ".control-label": {
+            "fontSize": `14px`,
+            "fontWeight": 500,
+        },
+        ".checkbox label": {
+            "color": "#637282",
+        },
+        "a": {
+            "color": "#637282",
+            ":hover" : {
+                "color": "#637282",
+            },
+        }
     },
     "kcFormHeaderClass": {
-        "& #kc-page-title": {
-            "fontWeight": `bold`,
+        "#kc-page-title": {
+            "fontSize": `24px`,
+            "fontWeight": 500,
+        },
+        "#kc-current-locale-link": {
+            "color": `#222`,
         },
     },
     "kcButtonPrimaryClass": {
+        "backgroundImage": `none`,
+        "border": `none`,
+        "box-shadow": `none`,
         "color": theme.primaryButtonTextColor,
         "backgroundColor": theme.primaryButtonColor,
-        "&:hover": {
-            "backgroundColor": theme.primaryButtonHoverColor
+        ":hover": {
+            "backgroundColor": theme.primaryButtonHoverColor,
+            "color": `#222222`,
         },
         "borderRadius": `4px`,
         "height": `48px`,
         "fontSize": `16px`,
-        "fontWeight": `500`,
+        "fontWeight": 500,
     },
     "kcLoginClass": {
         ".form-control": {
             "borderRadius": `4px`,
-            "height": `40px`,
-            "fontSize": `14px`,
+            "height": `48px`,
+            "fontSize": `16px`,
             "padding": `16px`,
             "backgroundColor": `#F8F9FB`,
             "borderColor": `#D6DBE4`,
             "color": `#222222`,
             "box-shadow": `none`,
-            "&:focus": {
+            ":focus": {
                 "backgroundColor": `#FFFFFF`,
                 "borderColor": `#194BDC`,
             },
-            "&::placeholder": {
+            "::placeholder": {
                 "fontStyle": `normal`,
                 "color": `#222 0.5`,
             }
